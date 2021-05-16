336120281736567f9562402be6c279c06eb305b1
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-16 13:23:29 +0530

    [Ranjith]added Emp_check.java

---
+++ Emp_check.java
@@ -0,0 +1,12 @@
+class Emp_check{
+	public static void main(String args[]){
+		int isFull=1;
+		double result=Math.floor(Math.random()*10)%2;
+		if(isFull==result){
+		System.out.println("the employee is full");
+		}
+		else{
+		System.out.println("the employee not full");
+		}
+	}
+}
